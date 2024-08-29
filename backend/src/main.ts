9b108c78a749a15aff4373cbd2897c2bcf291f4c
sweana <sweanaseaban@gmail.com>
2024-08-29 11:59:27 +0530

    Add cookie parser package

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,14 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3001', // Replace with the address of your React app
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+
+  // Enable CORS with the specified options
+  app.enableCors(corsOptions);
   const config = new DocumentBuilder()
     .setTitle('Users')
     .setDescription('The users API description')
